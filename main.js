7d0b8147875747c91bd504fb2c384c9070c7b277
Kaitlyn Tierney <k@krtierney.com>
2016-07-09 19:11:29 +0100

    Resized for retina screens

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ var canvas = document.getElementById('fox-game');
 var ctx = canvas.getContext('2d');
 window.onload = draw;
 
+canvas.width = 1600;
+canvas.height = 800;
+canvas.style.width = "800px";
+canvas.style.height = "400px";
+
 var rightArrowPress = false;
 var leftArrowPress = false;
 var upArrowPress = false;
@@ -21,21 +26,21 @@ foxImage.src = "assets/fox-night.png";
 
 var fox = {
     x: 0,
-    y: 300,
-    vx: 5, //reset to .5 after testing
-    vy: 1,
+    y: 600,
+    vx: 10, //reset to .5 after testing
+    vy: 2,
     draw: function() {
-      ctx.drawImage(foxImage, this.x, this.y, 160, 100);
+      ctx.drawImage(foxImage, this.x, this.y, 320, 200);
     }
 };
 
 var rectangle = {
- x: 800,
- y: 360,
- vx: -1,
+ x: 1600,
+ y: 720,
+ vx: -2,
  vy: 0,
  draw: function() {
-   ctx.fillRect (this.x,this.y,25,25);
+   ctx.fillRect (this.x,this.y,50,50);
  }
 };
 
@@ -47,84 +52,84 @@ function Obstacle(x, y, vx, vy) {
   this.vy = vy;
 }
 
-Obstacle.prototype.draw = function(image, xh, xy) {
-  ctx.drawImage(image, this.x, this.y, xh, xy);
+Obstacle.prototype.draw = function(image) {
+  ctx.drawImage(image, this.x, this.y);
 }
 
 var catImage = new Image();              
 catImage.src = "assets/cat.png"; 
-var cat = new Obstacle(630, 290, -.5, 0);
+var cat = new Obstacle(1260, 600, 0, 0);
 
 var mouseChatImage = new Image();              
 mouseChatImage.src = "assets/mouse-chat.png";  
-var mouseChat = new Obstacle(660,300,1,0)
+var mouseChat = new Obstacle(1320,750,-2,0)
 
 var mouseImage = new Image();              
 mouseImage.src = "assets/mouse.png";  
-var mouse = new Obstacle(670,350,-1,0)
+var mouse = new Obstacle(1600,700,-2, 0)
 
 var dogImage = new Image();              
 dogImage.src = "assets/dog.png";  
-var dog = new Obstacle(600,250,-1,0)
+var dog = new Obstacle(1200,500,-1,0)
 
 var owlFlyingImage = new Image();              
 owlFlyingImage.src = "assets/owl-flying.png";  
-var owlFlying = new Obstacle(10,10,-3,.1)
+var owlFlying = new Obstacle(0,0,3,.5)
 
 var owlImage = new Image();              
 owlImage.src = "assets/owl.png";  
-var owl = new Obstacle(548,190,.5,0)
+var owl = new Obstacle(1100,420,.1,0)
 
 var puddleImage = new Image();              
 puddleImage.src = "assets/puddle.png";  
-var puddle = new Obstacle(670,370,.5,0)
+var puddle = new Obstacle(1340,720,.1,0)
 
 var trashImage = new Image();              
 trashImage.src = "assets/trash.png";  
-var trash = new Obstacle(640,330,.5,0)
+var trash = new Obstacle(1280,660,.5,0)
 
 var shardsImage = new Image();              
 shardsImage.src = "assets/shards.png";  
-var shards = new Obstacle(600,350,.5,0)
+var shards = new Obstacle(1200,700,.5,0)
 
 
 //Draw a streetlamp constructor
 function StreetLamp(x, y, vx) {
-  this.x = x; //448
-  this.y = y; //196;
-  this.vx = vx; //.5;
+  this.x = x; //448*2
+  this.y = y; //196;*2
+  this.vx = vx; //.5;*2
   this.vy = 0;
 }
 
 StreetLamp.prototype.draw = function() {
   ctx.beginPath();
-  ctx.rect (this.x-30,this.y+50,10,100);
-  ctx.lineTo (this.x-48,this.y+14);
-  ctx.lineTo (this.x-24,this.y-10);
-  ctx.lineTo (this.x,this.y+14);
-  ctx.lineTo (this.x-20, this.y+54); 
+  ctx.rect (this.x-60,this.y+100,20,400);
+  ctx.lineTo (this.x-96,this.y+28);
+  ctx.lineTo (this.x-48,this.y-10);
+  ctx.lineTo (this.x,this.y+28);
+  ctx.lineTo (this.x-40, this.y+108); 
   ctx.fillStyle = "black";
   ctx.fill(); 
   ctx.beginPath();  
-  ctx.moveTo (this.x-26,this.y+44);
-  ctx.lineTo (this.x-22,this.y);
-  ctx.lineTo (this.x-44,this.y+15);
-  ctx.lineTo (this.x-29,this.y+46);  
-  ctx.lineTo (this.x-26,this.y+44);  
-  ctx.moveTo (this.x-23,this.y+44);
-  ctx.lineTo (this.x-20,this.y+2);
-  ctx.lineTo (this.x-4,this.y+14);
-  ctx.lineTo (this.x-20,this.y+46);  
-  ctx.lineTo (this.x-23,this.y+44);  
+  ctx.moveTo (this.x-52,this.y+88);
+  ctx.lineTo (this.x-44,this.y);
+  ctx.lineTo (this.x-88,this.y+30);
+  ctx.lineTo (this.x-58,this.y+92);  
+  ctx.lineTo (this.x-52,this.y+88);  
+  ctx.moveTo (this.x-46,this.y+88);
+  ctx.lineTo (this.x-40,this.y+4);
+  ctx.lineTo (this.x-8,this.y+28);
+  ctx.lineTo (this.x-40,this.y+92);  
+  ctx.lineTo (this.x-46,this.y+88);  
   ctx.fillStyle = "yellow"; 
   ctx.fill(); 
   ctx.fillStyle = "rgba(255,255,0,.1)";
   ctx.beginPath(); 
-  ctx.arc (this.x-26,this.y+34, 70, 0, 2 * Math.PI, false);
+  ctx.arc (this.x-52,this.y+68, 140, 0, 2 * Math.PI, false);
   ctx.fill();
 }
 
-var newLamp = new StreetLamp(548, 196, .5);
+var newLamp = new StreetLamp(1096, 197, 1);
 
 
 
@@ -140,40 +145,42 @@ DialogueBox.prototype.display = function(textToDisplay) {
   ctx.fillStyle = 'rgba(255,255,255,1)';
   ctx.fillRect (this.x, this.y, this.w, this.h);
   ctx.textAlign = 'center';
-  ctx.font = '20px Arial';
+  ctx.font = '30px Arial';
   ctx.fillStyle = 'black';
-  ctx.fillText (textToDisplay, this.x+250, this.y+30);
+  ctx.fillText (textToDisplay, this.x+500, this.y+60);
 
 }
 
-var first = new DialogueBox(150,50,500,50);
+var first = new DialogueBox(300,100,1000,100);
 
 
 
 
 //Logic for creating obstacles, including collision detection conditions
-function meetAnObstacle(obstacle, image, xh, xy) {
-  obstacle.draw(image, xh, xy);
+function meetAnObstacle(obstacle, image) {
+  obstacle.draw(image);
 }
 
 function collisionCheck(obstacle) {
-  var ox = obstacle.x += obstacle.vx;
-  var oy = obstacle.y += obstacle.vy;
-  var fx = fox.x;
-  var fy = fox.y+85;
-  if (((fx+110) >= ox) && ((fx-11) <= ox) && (fy >= oy)) {
-    obstacle.vx = 0;
-    collided = true;
-  };
-}
+    var ox = obstacle.x += obstacle.vx;
+    var oy = obstacle.y += obstacle.vy;
+    var fx = fox.x;
+    var fy = fox.y+170;
+    if (((fx+220) >= ox) && ((fx-22) <= ox) && (fy >= oy)) {
+      obstacle.vx = 0;
+      collided = true;
+    };
+  }
 
 function staticCollision(obstacle) {
-  var ox = obstacle.x += obstacle.vx;
-  var fx = fox.x;
-  var fy = fox.y+85;
-  if (((fx+110) >= ox) && ((fx-11) <= ox)) {
-    collided = true;
-  };
+  if (rightArrowPress) {
+    var ox = obstacle.x -= obstacle.vx;
+    var fx = fox.x;
+    var fy = fox.y+170;
+    if (((fx+220) >= ox) && ((fx-22) <= ox)) {
+      collided = true;
+    };
+  }
 }
 
 
@@ -193,13 +200,13 @@ function draw() {
   ctx.clearRect(0,0, canvas.width, canvas.height);
 
 //Starting screen
-  if (screenCount === 0 && fox.x>450) {
+  if (screenCount === 0 && fox.x>900) {
     ctx.clearRect(0,0, canvas.width, canvas.height);
     first.display("Where are you off to in such a hurry?");
-  } else if (screenCount === 0 && fox.x<400 && fox.x>250) {
+  } else if (screenCount === 0 && fox.x<800 && fox.x>500) {
       ctx.clearRect(0,0, canvas.width, canvas.height);
       first.display("Use the arrow keys to move.");
-  } else if (screenCount === 0 && fox.x<100 && fox.x>50) {
+  } else if (screenCount === 0 && fox.x<200 && fox.x>100) {
       first.display("Good evening, Fox");
    }
 
@@ -207,7 +214,7 @@ function draw() {
   newLamp.draw();
   
   if (screenCount === 1) {
-    meetAnObstacle(mouse,mouseImage,40,30);
+    meetAnObstacle(mouse,mouseImage);
     collisionCheck(mouse);
     if (collided === true) {
         first.display("You've found a mouse. Are you feeling hungry? Y/N");
@@ -227,13 +234,14 @@ function draw() {
   
   if (screenCount === 2 || screenCount === 4) {
     //owl flies wrong direction: fix
-    meetAnObstacle(owlFlying,owlFlyingImage,100,50);
-    collisionCheck;
+    meetAnObstacle(owlFlying,owlFlyingImage);
+    owlFlying.x += owlFlying.vx;
+    owlFlying.y += owlFlying.vy;
   };
 
   
   if (screenCount === 3) {
-    meetAnObstacle(trash,trashImage,70, 50);
+    meetAnObstacle(trash,trashImage);
     fixedObstacleShift(trash);
     staticCollision(trash);
     if (collided === true) {
@@ -243,7 +251,7 @@ function draw() {
 
   
   if (screenCount === 5) {
-    meetAnObstacle(puddle,puddleImage,50,30);
+    meetAnObstacle(puddle,puddleImage);
     fixedObstacleShift(puddle);
     staticCollision(puddle);
     if (collided === true) {
@@ -253,7 +261,7 @@ function draw() {
 
   
   if (screenCount === 7) {
-    meetAnObstacle(owl,owlImage,50,70);
+    meetAnObstacle(owl,owlImage);
     fixedObstacleShift(owl);
     staticCollision(owl);
     if (collided === true) {
@@ -263,7 +271,7 @@ function draw() {
 
   
   if (screenCount === 9) {
-    meetAnObstacle(dog,dogImage,210,140);
+    meetAnObstacle(dog,dogImage);
     collisionCheck(dog);
     if (collided === true) {
       first.display("'Grrr! Arf!'");
@@ -272,7 +280,7 @@ function draw() {
 
   
   if (screenCount === 10) {
-    meetAnObstacle(cat,catImage,50,70);
+    meetAnObstacle(cat,catImage);
     staticCollision(cat);
     if (collided === true) {
       first.display("Cat: Friend or Food? Y for friend, N for food.")
@@ -282,7 +290,7 @@ function draw() {
 
   
   if (screenCount === 11) {
-    meetAnObstacle(shards,shardsImage,50,30);
+    meetAnObstacle(shards,shardsImage);
     fixedObstacleShift(shards);
     staticCollision(shards);
     if (collided === true) {    
@@ -315,9 +323,9 @@ function keyPadControls() {
     fox.x -= fox.vx;
     newLamp.x += newLamp.vx;
   };
-  if (downArrowPress && fox.y<310) {
+  if (downArrowPress && fox.y<600) {
     fox.y += fox.vy;
-  } else if (upArrowPress && fox.y>270) {
+  } else if (upArrowPress && fox.y>520) {
     fox.y -= fox.vy;
   };
   if (fox.x > canvas.width) {
@@ -327,8 +335,8 @@ function keyPadControls() {
     screenCount++;
   };
 
-  if (newLamp.x < -20) {
-    newLamp.x = 805;
+  if (newLamp.x < -40) {
+    newLamp.x = 1610;
   };
 }
 
